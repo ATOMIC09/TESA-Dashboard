dcee353a2bcda051a69b2ccbd5a9082b18337def
I2eNamE <71267514+I2eNamE@users.noreply.github.com>
2024-11-14 10:08:28 +0700

    delete mqtt folder

--- rest_api/index.ts
+++ rest_api/index.ts
@@ -1,4 +1,6 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 
-const app =                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+const app = express()
+
+app.listen()
