eae35a9071ff9cc0a4a47d7a5ed58aae561e838c
I2eNamE <71267514+I2eNamE@users.noreply.github.com>
2024-11-14 12:13:22 +0700

    create post path to save data of sound from pi

---
+++ rest_api/controller/sound.ts
@@ -0,0 +1,57 @@
+import express from 'express'
+import database from '../config/db'
+import multer from "multer";
+import { logger } from '../config/logger'
+import { validateAPIKey } from './middleware'
+import { renameSync } from 'fs'
+import { file } from 'bun';
+import path from 'path';
+import { randomUUID } from 'crypto';
+
+const router = express.Router()
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req, file, cb) { cb(null, './static/sound') },
+        filename(req, file, callback) { callback(null, file.originalname) },
+    })
+})
+
+router.post("/", validateAPIKey, upload.single("file"), (req: express.Request, res: express.Response) => {
+    if (req.body instanceof Array) {
+        res.status(401).send({
+            error: "Invalid request"
+        })
+    }
+    const { deviceId, timeStamp } = req.body
+    const sound = req.file as Express.Multer.File
+
+    const uuid = randomUUID();
+    const filenameWithExt = path.parse(sound.originalname)
+    const filename = filenameWithExt.name
+    const ext = filenameWithExt.ext
+    const oldPath = `${sound.destination}/${sound.originalname}`
+    const newPath = `${sound.destination}/${filename}_${uuid}${ext}`
+    logger.debug(`Old path : ${oldPath}`);
+    logger.debug(`New path : ${newPath}`);
+    renameSync(oldPath, newPath)
+    
+
+    const sounddata = {
+        deviceId, timeStamp, filePath:newPath.slice(1)
+        }
+    const col = database.collection('sound')
+
+    try {
+        const data = col.insertOne(sounddata)
+        res.status(201).send({ message: "OK", ids: data })
+    }
+    catch (error) {
+        logger.error(`${error}`);
+        res.status(500).send({ error: `${error}` })
+    }
+})
+
+
+export default router
+
