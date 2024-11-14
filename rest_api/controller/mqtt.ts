9b195adb1d9866d8ae51ec15103a897a03b224ea
I2eNamE <71267514+I2eNamE@users.noreply.github.com>
2024-11-14 20:19:50 +0700

    set topic to subcribe

--- rest_api/controller/mqtt.ts
+++ rest_api/controller/mqtt.ts
@@ -4,7 +4,7 @@ const user = process.env.user
 const password = process.env.password
 
 const brokerurl = process.env.mqtturl as string
-const topics = ['model/ticker'];
+
 
 
 const client = mqtt.connect(brokerurl,{username:user,password})
