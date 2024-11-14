309c9f1f66694527962c53eb891be115c9d61806
SkyJTx <nattakarn.khumsupha.user1@outlook.com>
2024-11-14 23:53:21 +0700

    fix seg fault and mem leak

--- hw/ml_unit.c
+++ hw/ml_unit.c
@@ -1,44 +1,63 @@
 #include "main.h"
 
 void* ml_unit() {
-
-    char command[100] = "TEST Command\0";
-    char* command_copy = malloc(sizeof(char) * 100);
+    char ccc[] = {'F', 'N', 'X'};
+    int length = sizeof(ccc) / sizeof(ccc[0]);
+    char command[1024] = "TEST Command\0";
+    char command_copy[1024];
+    time_t t;
     while (1) {
-        get_shared_var(&command_data, sizeof(char) * 100, command_copy);
-        if (strcmp(command_copy, command) != 0) {
-            printf("Command received: %s\n", command_copy);
-            strcpy(command, command_copy);
+        // get_shared_var(&command_data, sizeof(char) * 100, command_copy);
+        // pthread_mutex_lock(&command_data.lock);
+        // strcpy(command_copy, command_data.shared_var);
+        // pthread_mutex_unlock(&command_data.lock);
 
-            // Do something with the command
-            if (strcmp(command, "update-model") == 0) {
-                // Update the model
-            }
-        }
+        // if (strcmp(command_copy, command) != 0) {
+        //     printf("Command received: %s\n", command_copy);
+        //     strcpy(command, command_copy);
+
+        //     // Do something with the command
+        //     if (strcmp(command, "update-model") == 0) {
+        //         // Update the model
+        //     }
+        // }
 
         // Do Classification
+        // double x[48000];
+        // for (int i = 0; i < 48000; i++) {
+        //     x[i] = (double)rand() / RAND_MAX;
+        // }
 
-        time_t t = time(NULL);
+        time(&t);
         struct tm tm = *localtime(&t);
         char created_at[100];
         strftime(created_at, 26, "%Y-%m-%dT%H:%M:%S%z", &tm);
 
         int timestamp = (int)t;
-        char* classification = "X";
-        double confidence = 0.99;
+        int rI = rand() % length;
+        char classification = ccc[rI];
+        char classification_copy[1];
+
+        memcpy(classification_copy, &classification, sizeof(char));
+        double confidence = (double)rand() / RAND_MAX;
 
         cJSON *root = cJSON_CreateObject();
         cJSON_AddNumberToObject(root, "timestamp", timestamp);
         cJSON_AddStringToObject(root, "created_at", created_at);
-        cJSON_AddStringToObject(root, "classification", classification);
+        cJSON_AddStringToObject(root, "classification", classification_copy);
         cJSON_AddNumberToObject(root, "confidence", confidence);
         char *payload = cJSON_PrintUnformatted(root);
 
-        printf("Payload: %s\n", payload);
+        pthread_mutex_lock(&ml_sender_data.lock);
+        strcpy(ml_sender_data.shared_var, payload);
+        pthread_cond_signal(&ml_sender_data.cond);
+        pthread_mutex_unlock(&ml_sender_data.lock);
+        // set_shared_var_cond_signal(&ml_sender_data, payload, sizeof(char) * 1024);
 
-        set_shared_var_cond_signal(&ml_sender_data, payload, sizeof(char) * 1024);
+        cJSON_Delete(root);
+        free(payload);
         
-        sleep(1);
+        // sleep(1);
 
 
     }
