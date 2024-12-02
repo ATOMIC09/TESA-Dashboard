91ea49041212ed35f346e66f4728dce12432483a
Phutawan Chanrueng <phutawanchanrueng@gmail.com>
2024-12-02 21:22:19 +0700

    Update config.ts

--- dashboard/src/app/server/config.ts
+++ dashboard/src/app/server/config.ts
@@ -1,1 +1,1 @@
-export const NEXT_PUBLIC_BACKENDSERVER = process.env.NEXT_PUBLIC_BACKENDSERVER || 'http://localhost:8080';
+export const NEXT_PUBLIC_BACKENDSERVER = process.env.NEXT_PUBLIC_BACKENDSERVER;
