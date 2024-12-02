28a77b92f1a9813119050ee38249c24286abdac3
Phutawan Chanrueng <phutawanchanrueng@gmail.com>
2024-12-02 17:37:59 +0700

    Rename env

--- rest_api/controller/mqtt.ts
+++ rest_api/controller/mqtt.ts
@@ -3,7 +3,7 @@ import mqtt from "mqtt"
 const user = process.env.NEXT_PUBLIC_MQTT_USERNAME
 const password = process.env.NEXT_PUBLIC_MQTT_PASSWORD
 
-const brokerurl = process.env.mqtturl as string
+const brokerurl = process.env.MQTT_URL as string
 
 
 
